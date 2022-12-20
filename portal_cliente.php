2a31208ec4d78eb7960edf4766b2cea9a2654c1b
MatheusEduardo <45131659+MatheusEduardoSilvaOliveira@users.noreply.github.com>
2022-12-20 17:16:25 -0300

    my first commit

---
+++ portal_cliente.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Portal do Cliente</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css" />     
+<link rel="stylesheet" type="text/css" href="css/isotope.css" media="screen" />	
+<link rel="stylesheet" href="js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+<link rel="stylesheet" type="text/css" href="css/da-slider.css" />
+<link rel="stylesheet" href="css/styles.css"/>
+<!-- Font Awesome -->
+<link href="font/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+	<header class="header">
+	<div class="container">
+		<nav class="navbar navbar-inverse" role="navigation">
+        	<div class="navbar-header">
+	            <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
+		            <span class="sr-only">Toggle navigation</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+	            </button>
+	            <a href="#" class="navbar-brand scroll-top logo"><img src="images/logo_c.png" alt="skytouch" /></a>
+        	</div>
+        	<!--/.navbar-header-->
+	        <div id="main-nav" class="collapse navbar-collapse">
+	            <ul class="nav navbar-nav" id="mainNav">
+		            <li class="active"><a href="#home" class="scroll-link">Home</a></li>
+					<li><a href="#aboutUs" class="scroll-link">Sobre Nós</a></li>
+					<li><a href="#services" class="scroll-link">Serviços</a></li>
+					<li><a href="#team" class="scroll-link">Time</a></li>
+					<li><a href="#contactUs" class="scroll-link">Contato</a></li> 
+	            </ul>
+	        </div>
+	        <!--/.navbar-collapse-->
+	    </nav>
+	    <!--/.navbar-->
+	</div>
+	<!--/.container-->
+</header>
+<div class="container">
+<h2>Portal do Cliente</h2>
+<a href="index.html" class="da-link button-outline">Voltar</a>
+	</div>
+</body>
+</html>
